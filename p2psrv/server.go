785e38af4de4afb8a9b3d4f28c317f2d7ad283c3
tony <liboliqi@gmail.com>
2020-07-02 16:51:29 +0800

    copy discv5 from go-ethereum

--- p2psrv/server.go
+++ p2psrv/server.go
@@ -13,11 +13,11 @@ import (
 	"github.com/ethereum/go-ethereum/common/mclock"
 	"github.com/ethereum/go-ethereum/p2p"
 	"github.com/ethereum/go-ethereum/p2p/discover"
-	"github.com/ethereum/go-ethereum/p2p/discv5"
 	"github.com/ethereum/go-ethereum/p2p/nat"
 	"github.com/inconshreveable/log15"
 	"github.com/vechain/thor/cache"
 	"github.com/vechain/thor/co"
+	"github.com/vechain/thor/p2psrv/discv5"
 )
 
 var log = log15.New("pkg", "p2psrv")
@@ -173,18 +173,14 @@ func (s *Server) listenDiscV5() (err error) {
 		return err
 	}
 
-	realaddr := conn.LocalAddr().(*net.UDPAddr)
 	if s.opts.NAT != nil {
+		realaddr := conn.LocalAddr().(*net.UDPAddr)
 		if !realaddr.IP.IsLoopback() {
 			s.goes.Go(func() { nat.Map(s.opts.NAT, s.done, "udp", realaddr.Port, realaddr.Port, "vechain discovery") })
 		}
-		// TODO: react to external IP changes over time.
-		if ext, err := s.opts.NAT.ExternalIP(); err == nil {
-			realaddr = &net.UDPAddr{IP: ext, Port: realaddr.Port}
-		}
 	}
 
-	network, err := discv5.ListenUDP(s.opts.PrivateKey, conn, realaddr, "", s.opts.NetRestrict)
+	network, err := discv5.ListenUDP(s.opts.PrivateKey, conn, "", s.opts.NetRestrict)
 	if err != nil {
 		return err
 	}
