0c42a09bc996f4ab742bbcc320ce6773aff0ee19
tony <liboliqi@gmail.com>
2026-03-20 16:23:30 +0800

    bump vechain/go-ethereum dep

--- thor/hash_test.go
+++ thor/hash_test.go
@@ -12,7 +12,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/crypto/sha3"
+	"github.com/ethereum/go-ethereum/crypto/keccak"
 )
 
 func BenchmarkHash(b *testing.B) {
@@ -29,7 +29,7 @@ func BenchmarkHash(b *testing.B) {
 			Read([]byte) (int, error)
 		}
 
-		k := sha3.NewLegacyKeccak256().(keccakState)
+		k := keccak.NewLegacyKeccak256().(keccakState)
 		var b32 Bytes32
 		for b.Loop() {
 			k.Write(data)
