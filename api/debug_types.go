d4f705a5b09f0b6eee67b04ccaca7ee9c8a4db9d
tony <liboliqi@gmail.com>
2026-08-04 17:01:27 +0800

    refactor: move debug wire types to api/dto

--- api/debug_types.go
+++ api/debug_types.go
@@ -5,50 +5,13 @@
 
 package api
 
-import (
-	"encoding/json"
+import "github.com/vechain/thor/v2/api/dto"
 
-	"github.com/ethereum/go-ethereum/common/math"
-
-	"github.com/vechain/thor/v2/thor"
+type (
+	TraceClauseOption  = dto.TraceClauseOption
+	TraceCallOption    = dto.TraceCallOption
+	StorageRangeOption = dto.StorageRangeOption
+	StorageRangeResult = dto.StorageRangeResult
+	StorageMap         = dto.StorageMap
+	StorageEntry       = dto.StorageEntry
 )
-
-type TraceClauseOption struct {
-	Name   string          `json:"name"`
-	Target string          `json:"target"`
-	Config json.RawMessage `json:"config"` // Config specific to given tracer.
-}
-
-type TraceCallOption struct {
-	To         *thor.Address         `json:"to"`
-	Value      *math.HexOrDecimal256 `json:"value"`
-	Data       string                `json:"data"`
-	Gas        uint64                `json:"gas"`
-	GasPrice   *math.HexOrDecimal256 `json:"gasPrice"`
-	ProvedWork *math.HexOrDecimal256 `json:"provedWork"`
-	Caller     *thor.Address         `json:"caller"`
-	GasPayer   *thor.Address         `json:"gasPayer"`
-	Expiration uint32                `json:"expiration"`
-	BlockRef   string                `json:"blockRef"`
-	Name       string                `json:"name"`   // Tracer
-	Config     json.RawMessage       `json:"config"` // Config specific to given tracer.
-}
-
-type StorageRangeOption struct {
-	Address   thor.Address
-	KeyStart  string
-	MaxResult int
-	Target    string
-}
-
-type StorageRangeResult struct {
-	Storage StorageMap    `json:"storage"`
-	NextKey *thor.Bytes32 `json:"nextKey"` // nil if Storage includes the last key in the trie.
-}
-
-type StorageMap map[string]StorageEntry
-
-type StorageEntry struct {
-	Key   *thor.Bytes32 `json:"key"`
-	Value *thor.Bytes32 `json:"value"`
-}
